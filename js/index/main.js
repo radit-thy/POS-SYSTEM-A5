25abbdc0b65893f3c4cedb037e47e9f94271534a
ROTH SAMRETH <rothsamreth@student.passerellesnumeriques.org>
2023-12-11 15:43:43 +0700

    #5 update

--- js/index/main.js
+++ js/index/main.js
@@ -1,14 +1,21 @@
-// 
-let data = [{name:"",category:"",price:"",quantity:"",popularity:""}]
+//
+let products = [
+  { name: "", category: "", price: "", quantity: "", popularity: "" },
+];
 //drop down
 const categories = document.querySelector(".categories");
 const cat = document.querySelector(".dropdown");
 categories.addEventListener("click", () => {
   cat.classList.toggle("drop-category");
 });
-
+// Save Product to Local
+function loadToLocal() {
+  localStorage.setItem("products", JSON.stringify(products));
+}
+loadToLocal();
 // all variable
-
+const cardContainer = document.querySelector(".card-container");
+const cartContainer = document.querySelector(".cart-container");
 // add new product
 
 // add new category

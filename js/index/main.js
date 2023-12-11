f049973914075392b653f115286c4b5200afaba2
ROTH SAMRETH <rothsamreth@student.passerellesnumeriques.org>
2023-12-11 14:28:38 +0700

    #5 add some changes

--- js/index/main.js
+++ js/index/main.js
@@ -1,5 +1,22 @@
+// 
+let data = [{name:"",category:"",price:"",quantity:"",popularity:""}]
+//drop down
 const categories = document.querySelector(".categories");
 const cat = document.querySelector(".dropdown");
 categories.addEventListener("click", () => {
   cat.classList.toggle("drop-category");
 });
+
+// all variable
+
+// add new product
+
+// add new category
+
+// display product
+
+// display category
+
+// remove product
+
+// remove category

126730d603ddc04765de706241e1ba2abf8b90ba
CEO of Bathroom <141013479+chhuneii2004@users.noreply.github.com>
2023-12-13 19:57:21 +0700

    New code for delete catagory

--- js/admin/main.js
+++ js/admin/main.js
@@ -1,3 +1,4 @@
+// remove on Action
 let reMoves = document.querySelectorAll(".remove");
 
 function reMoveOnAction(event) {
@@ -9,4 +10,17 @@ function reMoveOnAction(event) {
 
 for (let reMove of reMoves) {
   reMove.addEventListener("click", reMoveOnAction);
+}
+
+// remove catagory
+let reMOVES = document.querySelectorAll(".remove-category")
+
+function reMoveCatagory(event){
+ 
+  let clickedOnButtonRemove = event.target;
+  clickedOnButtonRemove.parentNode.removeChild(clickedOnButtonRemove);
+
+}
+for (let reMOVE of reMOVES){
+  reMOVE.addEventListener("click",reMoveCatagory);
 }

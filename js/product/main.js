29adb4239e474ba804f6ee64342baeb6805b0743
ROTH SAMRETH <rothsamreth@student.passerellesnumeriques.org>
2023-12-15 16:32:49 +0700

    add product feature halfway

--- js/product/main.js
+++ js/product/main.js
@@ -8,88 +8,62 @@ let productItem = [
   //     sellStaus: "2",
   //   },
 ];
-let reMoves = document.querySelectorAll(".remove");
-const menuBtn = Array.from(document.querySelectorAll("aside button"));
-const customerRecord = document.querySelector(".customer-record");
-const producManagement = document.querySelector(".products-management");
-const categoryManagement = document.querySelector(".category-management");
-const productName = document.getElementById("product-name");
-const productCategory = document.getElementById("product-category");
-const productPrice = document.getElementById("product-price");
-const productImage = document.getElementById("product-image");
-const productQuantity = document.getElementById("product-quantity");
-const addProductBtn = document.getElementById("add");
-const productTable = document.getElementById("p-management-table");
-// set variable
-
-// Save Product to Local
-function loadToLocal(data) {
-  localStorage.setItem("productItems", JSON.stringify(data));
-}
-function getData() {
-  productItem = JSON.parse(localStorage.getItem("productItems"));
-}
-// loadToLocal(productItem);
-// function readImg(image) {
-//   let img = new FileReader();
-//   img.readAsDataURL(productImage.files[0]);
-//   productImage.addEventListener("onchange", () => {
-//     image.setAttribute("src", img.result);
-//   });
-// }
-function getDataFromInput() {
-  let data = {};
-  if (productName.checkValidity()) {
-    data.name = productName.value;
-    data.productImg = productImage.files[0];
-    data.category = productCategory.value;
-    data.price = productPrice.value;
-    data.quantity = productQuantity.value;
-    productItem.push(data);
-    loadToLocal(productItem);
+// form
+const pName = document.getElementById("product-name");
+const pCategory = document.getElementById("product-category");
+const pPrice = document.getElementById("product-price");
+const pQuantity = document.getElementById("product-quantity");
+// const tableBody = document.getElementById("p-management-table");
+const addBtn = document.getElementById("add");
+let id = 0;
+function createTableRow(e) {
+  e.preventDefault();
+  let allItem = tableBody.children;
+  let notExist = true;
+  for (let item of allItem) {
+    if (pName.value === item.children[1].textContent) {
+      notExist = false;
+      item.children[4].firstChild.textContent =
+        parseInt(item.children[4].firstChild.textContent) +
+        parseInt(pQuantity.value);
+      item.children[3].textContent =
+        parseInt(item.children[4].firstChild.textContent) *
+        parseInt(pPrice.value);
+      console.log(pQuantity.value);
+    }
+  }
+  if (notExist && pName.value !== "") {
+    const tableRow = document.createElement("tr");
+    const tdId = document.createElement("td");
+    const tdName = document.createElement("td");
+    const tdCategory = document.createElement("td");
+    const tdPrice = document.createElement("td");
+    const tdQuantity = document.createElement("td");
+    const spanQuantity = document.createElement("span");
+    const tdAction = document.createElement("td");
+    const tdAction1 = document.createElement("td");
+    const editBtn = document.createElement("button");
+    const removeBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    removeBtn.textContent = "Remove";
+    tdId.textContent = id;
+    tdName.textContent = pName.value;
+    tdCategory.textContent = pCategory.value;
+    tdPrice.textContent = pPrice.value;
+    spanQuantity.textContent = pQuantity.value;
+    tdQuantity.appendChild(spanQuantity);
+    tdAction.appendChild(editBtn);
+    tdAction1.appendChild(removeBtn);
+    tableRow.appendChild(tdId);
+    tableRow.appendChild(tdName);
+    tableRow.appendChild(tdCategory);
+    tableRow.appendChild(tdPrice);
+    tableRow.appendChild(tdQuantity);
+    tableRow.appendChild(tdAction);
+    tableRow.appendChild(tdAction1);
+    tableBody.appendChild(tableRow);
+    id++;
   }
 }
-let i = 0;
-addProductBtn.addEventListener("click", getDataFromInput);
-
-function createTable(data) {
-  const tableRow = document.createElement("tr");
-  const tdId = document.createElement("td");
-  const tdName = document.createElement("td");
-  const tdQuantity = document.createElement("td");
-  const tdEdit = document.createElement("td");
-  const editBtn = document.createElement("button");
-  editBtn.className = "edit";
-  const tdRemove = document.createElement("td");
-  const removeBtn = document.createElement("button");
-  removeBtn.classList.add("table-remove");
-  tdId.textContent = i;
-  tdName.textContent = data.name;
-  tdQuantity.textContent = data.quantity + " Unit(s)";
-  editBtn.textContent = "Edit";
-  removeBtn.textContent = "Remove";
-  removeBtn.setAttribute("id", i);
-  tdEdit.appendChild(editBtn);
-  tdRemove.appendChild(removeBtn);
-  tableRow.appendChild(tdId);
-  tableRow.appendChild(tdName);
-  tableRow.appendChild(tdQuantity);
-  tableRow.appendChild(tdEdit);
-  tableRow.appendChild(tdRemove);
-  productTable.appendChild(tableRow);
-  i++;
-}
-
-getData();
-for (let item of productItem) {
-  createTable(item);
-}
-const removeProductBtn = document.querySelectorAll(".table-remove");
-console.log(removeProductBtn);
-for (let btn of removeProductBtn) {
-  btn.addEventListener("click", () => {
-    productItem.splice(btn.id, 1);
-    loadToLocal(productItem);
-    btn.parentElement.parentElement.remove();
-  });
-}
+const tableBody = document.getElementById("p-management-table");
+addBtn.addEventListener("click", createTableRow);
